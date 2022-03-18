e0226c1ab39f55e34cc22b2a7a68e619306d948d
Carlos Peña <cpena3@u.rochester.edu>
2022-03-18 13:04:05 -0400

    clean up comments

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const dns = require("dns");
 const URL = require("url").URL;
 const port = process.env.PORT || 3000;
 
@@ -30,25 +29,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/api/shorturl", async (req, res) => {
   const o_url = req.body.url;
-  //o_url_string = o_url.toString();
+
   const s_url = Math.floor(Math.random() * 1000);
   // const regex =
   //  "^((http|https)://)(www.)?(?!.*(http|https|www.))[a-zA-Z0-9_-]+(.[a-zA-Z]+)+((/)[w#]+)*(/w+?[a-zA-Z0-9_]+=w+(&[a-zA-Z0-9_]+=w+)*)?/?$";
   try {
     console.log("reqbody: " + o_url);
     const url = new URL(o_url);
-    //if (!req.body.url.match(regex)) {
+
     if (url.protocol === "ftp:") {
       res.json({
         error: "invalid url",
       });
     } else {
-      // console.log("url Obj: " + urlObj.hostname);
-      // dns.lookup(o_url_string, async (err) => {
-      // if (err) {
-      //   console.log(err);
-      //   res.json({ error: "invalid url" });
-      // }
       let findOne = await Url.findOne({ original_url: o_url });
 
       if (findOne) {
@@ -69,7 +62,6 @@ app.post("/api/shorturl", async (req, res) => {
           short_url: findOne.short_url,
         });
       }
-      // });
     }
   } catch (err) {
     if (err) {
